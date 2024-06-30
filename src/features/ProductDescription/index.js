d0281d797ada1f02af73c54f218d6e0bcd682813
foryou929 <zxcvwert3457@gmail.com>
2024-06-30 16:29:36 +0900

    feat: add-product-description-page

---
+++ src/features/ProductDescription/index.js
@@ -0,0 +1,11 @@
+const ProductDescription = () => {
+    return (
+        <>
+            <div className="flex flex-wrap">
+
+            </div>
+        </>
+    );
+}
+
+export default ProductDescription;
